12207d9357f72d7d8cb37c2689aa3a6878556b31
Lawrence <me@merklejerk.com>
2018-10-22 13:45:38 -0400

    Mon Oct 22 13:45:38 EDT 2018

---
+++ sol/UpcityMarket.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.23;
+
+import './base/open-zeppelin/SafeMath.sol';
+import './base/bancor/BancorFormula.sol';
+
+using SafeMath for uint256;
+
+/// @title The bancor market(s) for UpCity's resources.
+contract UpcityMarket {
+
+}
